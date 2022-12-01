e7f390db8b51fbe3ef35a16d32373e5e61512958
Insaneboy9 <2717941S@student.gla.ac.uk>
2022-12-02 00:34:53 +0800

    modularise

--- format.py
+++ format.py
@@ -9,28 +9,8 @@ def list_paths(path):
     non_empty_dirs = [x for x in directories if x] # filter out empty lists
     return [item for subitem in non_empty_dirs for item in subitem] # flatten the list
 
-# Load the xlsx file
-pathToExcel="C://Users//Daryl//Documents//Coding_projects//Python//testing//python test//tracker.xlsx"
-excel_data = pd.read_excel(pathToExcel)
-data = pd.DataFrame(excel_data, columns=['Number', 'PIC', 'Original Date','Internal email', 'Outside email','Number email', 'Upload','Upload date', 'Today date', 'Comments'])
-
-#Load dataset
-pathToDataset = "C://Users//Daryl//Documents//Coding_projects//Python//testing//python test//dataset"
-folders=os.listdir(pathToDataset)
-
-#Store all folders in a list, then format the names according to excel 
-foldersList=list_paths(pathToDataset)
-formattedFoldersList=[]
-for folder in foldersList:
-    formattedFoldersList.append(folder.split(' ',1)[0])
-
-
-#Editing Excel File
-for row in data.index:
-    if len(str(data['Original Date'][row]))>3: #To check if original date is empty, 3 because empty means 'NAN' 
-        continue
-    else:
-        for folder in foldersList:
+def edit_dataframe(foldersList,data,pathToDataset):
+    for folder in foldersList:
             if folder.split(' ',1)[0]==data['Number'][row] or (folder.split(' ',1)[0]+' ' + folder.split(' ',2)[1])==data['Number'][row]:
                 currentDir=os.path.join(pathToDataset,folder)
                 for filename in os.listdir(currentDir):
@@ -47,6 +27,25 @@ for row in data.index:
                 if data['Number email'][row] != 'Yes':
                     data.at[row,"Number email"]= 'No'
                 break
+
+# Load the xlsx file
+pathToExcel="C://Users//Daryl//Documents//Coding_projects//Python//testing//python test//tracker.xlsx"
+excel_data = pd.read_excel(pathToExcel)
+data = pd.DataFrame(excel_data, columns=['Number', 'PIC', 'Original Date','Internal email', 'Outside email','Number email', 'Upload','Upload date', 'Today date', 'Comments'])
+
+#Load dataset
+pathToDataset = "C://Users//Daryl//Documents//Coding_projects//Python//testing//python test//dataset"
+folders=os.listdir(pathToDataset)
+
+#Store all folders in a list, then format the names according to excel 
+foldersList=list_paths(pathToDataset)
+
+#Editing Excel File
+for row in data.index:
+    if len(str(data['Original Date'][row]))>3: #To check if original date is empty, 3 because empty means 'NAN' 
+        continue
+    else:
+        edit_dataframe(foldersList,data,pathToDataset)
 
 data.to_excel('UpdatedTracker.xlsx', index=False)
 print("You have successfully updated the tracker! ")
